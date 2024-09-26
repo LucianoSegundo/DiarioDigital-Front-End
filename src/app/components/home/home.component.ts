4096f65429c2449b1e10659c83ae32eced21c0ff
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-09-25 22:17:15 -0300

    conclusão do cadastro

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,53 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { ButtonComponent } from "../button/button.component";
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UsuarioRequest } from '../../DTO/UsuarioRequest';
+import { AcessoApiService } from '../../service/acesso-api.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ButtonComponent],
+  imports: [HeaderComponent, ButtonComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-   resposta:boolean = false;
+  erroForm: boolean = false;
+  mensagemErro: string = "";
+  suceesoform: boolean = false;
+
+  constructor(private api: AcessoApiService) { }
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    senha: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    palavraSegu: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    idade: new FormControl(0, [Validators.required, Validators.min(18)]),
+    confsenha: new FormControl('', [Validators.required, Validators.minLength(4)])
+  });
+
+  Cadastrar() {
+    if (this.formulario.valid) {
+
+      if (this.formulario.value.senha == this.formulario.value.confsenha) {
+
+        this.api.cadastrarUsu(this.formulario.value as UsuarioRequest).subscribe({
+          next: (data) => {
+            console.log('Dados recebidos:', data);
+            this.suceesoform = true;
+            this.formulario.reset();
+          },
+          error: (error) => {
+            console.error('Erro ao fazer a requisição:', error.error.message);
+            this.mensagemErro = error.error.message;
+            this.erroForm = true;
+          }
+
+        });
+
+      }
+      else this.formulario.value.confsenha = null;
+
+    }
+  }
 
 }
