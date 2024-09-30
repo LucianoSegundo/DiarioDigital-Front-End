b0e3764b2f6f4cb710205949d7ff80375b753da4
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2024-09-30 11:02:38 -0300

    inicio do desenvolvimento da tela principal

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
 export class PrincipalComponent {
+ clique:boolean = true;
+ falha:boolean = false;
+ aguardando:boolean = false;
+ mensagemErro:string = "";
+
+ formulario = new FormGroup({
+  titulo: new FormControl('', [Validators.required, Validators.minLength(4)]),
+
+});
+ campoTitulo:string =  "borverde";
+ ConferirCampoPrinci(alvo:string){
+  if (this.formulario.get(alvo)?.hasError("required") || this.formulario.get(alvo)?.hasError("minlength")) {
+    if (alvo == "titulo") this.campoTitulo = "borVer"; } 
+    else { this.campoTitulo = "borverde";}
+}
+
+  CriarLivro(){}
 
 }
